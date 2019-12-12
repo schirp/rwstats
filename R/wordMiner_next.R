8ab098525cfce6faf0a05e5a59c94f763746319d
flariet <flariet@github.com>
2019-12-12 08:32:36 +0800

    fix \dontrun

--- R/wordMiner_next.R
+++ R/wordMiner_next.R
@@ -10,8 +10,8 @@
 #' @export
 #' @examples
 #' \dontrun{
-#' wordMiner.next("知")
-#' wordMiner.next("有", 20)
+#' wordMiner.next(strsplit(levels(twoChar$character)[988], "")[[1]][1])
+#' wordMiner.next(strsplit(levels(twoChar$character)[988], "")[[1]][2], 20)
 #' }
 wordMiner.next <- function(candidateWord, topN = 10) {
   if(length(strsplit(candidateWord, '')[[1]]) != 1) {
